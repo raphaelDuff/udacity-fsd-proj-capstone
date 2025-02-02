d56ae066cd4decc8da9c4924ca1a14f9cb3a45f2
raphaelDuff <raphaelprates@outlook.com>
2025-02-02 15:33:01 -0300

    Fixing some logic related to the many to many relationship

--- app/backend/src/api.py
+++ app/backend/src/api.py
@@ -63,24 +63,24 @@ def create_app(test_config=None, db=db):
         # db.session.commit()
 
     # ROUTES
-    # @app.route("/drinks", methods=["GET"])
-    # def get_drinks():
-    #     stmt_select_all_drinks = select(Drink).order_by(Drink.id)
-    #     drinks = db.session.scalars(stmt_select_all_drinks).all()
-    #     list_drinks = [drink.short() for drink in drinks]
-    #     if len(drinks) == 0:
-    #         abort(404)
-    #     return jsonify({"success": True, "drinks": list_drinks})
 
-    # @app.route("/drinks-detail", methods=["GET"])
+    # @app.route("/movie-details/<int:id>", methods=["GET"])
     # # @requires_auth("get:drinks-detail")
-    # def get_drinks_detail(token):
-    #     stmt_select_all_drinks = select(Drink).order_by(Drink.id)
-    #     drinks = db.session.scalars(stmt_select_all_drinks).all()
-    #     list_drinks = [drink.long() for drink in drinks]
-    #     if len(drinks) == 0:
+    # def get_drinks_detail(id):
+    #     stmt_movie_by_id = select(Movie).where(Movie.id == id)
+    #     selected_movie = db.session.scalars(stmt_movie_by_id).one_or_none()
+    #     if selected_movie is None:
     #         abort(404)
-    #     return jsonify({"success": True, "drinks": list_drinks})
+    #     return jsonify({"success": True, "movie": list_drinks})
+
+    @app.route("/movies", methods=["GET"])
+    def get_movies():
+        stmt_select_all_movies = select(Movie).order_by(Movie.id)
+        movies = db.session.scalars(stmt_select_all_movies).all()
+        list_movies = [movie.short() for movie in movies]
+        if len(movies) == 0:
+            abort(404)
+        return jsonify({"success": True, "movies": list_movies})
 
     @app.route("/movies", methods=["POST"])
     def post_movie():
@@ -93,30 +93,33 @@ def create_app(test_config=None, db=db):
             data_json_release_date = data_json.get("release_date", None)
             data_json_actors = data_json.get("actors", None)
 
-            if data_json_movie_title is None or data_json_release_date is None:
+            if (
+                data_json_movie_title is None
+                or data_json_release_date is None
+                or data_json_actors is None
+            ):
                 abort(400)
 
             release_date_datetime = datetime.strptime(
                 str(data_json_release_date), "%d-%m-%Y"
             )
 
-            if data_json_actors is None:
-                new_movie = Movie(
-                    title=data_json_movie_title, release_date=release_date_datetime
-                )
-            else:
+            actors = []
+
+            if len(data_json_actors) > 0:
                 stmt_select_actors_by_ids = select(Actor).where(
                     Actor.id.in_(data_json_actors)
                 )
-                actors_by_id_list = db.session.scalars(stmt_select_actors_by_ids).all()
-                new_movie = Movie(
-                    title=data_json_movie_title,
-                    release_date=release_date_datetime,
-                    actors=actors_by_id_list,
-                )
+                actors = db.session.scalars(stmt_select_actors_by_ids).all()
 
+            new_movie = Movie(
+                title=data_json_movie_title,
+                release_date=release_date_datetime,
+                actors=actors,
+            )
             db.session.add(new_movie)
             db.session.commit()
+
             return (
                 jsonify({"success": True, "movie": new_movie.short()}),
                 200,
@@ -139,18 +142,33 @@ def create_app(test_config=None, db=db):
             if not data_json:
                 abort(400)
 
-            selected_movie.title = data_json.get("title", selected_movie.title)
-            data_json_release_date = data_json.get(
-                "release_date", selected_movie.release_date
-            )
+            data_json_movie_title = data_json.get("title", None)
+            data_json_release_date = data_json.get("release_date", None)
+            data_json_actors = data_json.get("actors", None)
+
+            if (
+                data_json_movie_title is None
+                or data_json_release_date is None
+                or data_json_actors is None
+            ):
+                abort(400)
 
             selected_movie.release_date = datetime.strptime(
                 str(data_json_release_date), "%d-%m-%Y"
             )
 
-            # TODO - first retrieve the MOVIES LIST to check if there is any before update the value
-            # selected_actor.movies = data_json.get("movies", selected_actor.movies)
+            selected_movie.actors = []
 
+            if len(data_json_actors) > 0:
+                stmt_select_actors_by_ids = select(Actor).where(
+                    Actor.id.in_(data_json_actors)
+                )
+                selected_movie.actors = db.session.scalars(
+                    stmt_select_actors_by_ids
+                ).all()
+
+            selected_movie.title = data_json_movie_title
+            selected_movie.release_date = data_json_release_date
             db.session.add(selected_movie)
             db.session.commit()
             return (
@@ -186,6 +204,15 @@ def create_app(test_config=None, db=db):
         finally:
             db.session.close()
 
+    @app.route("/actors", methods=["GET"])
+    def get_actors():
+        stmt_select_all_actors = select(Actor).order_by(Actor.id)
+        actors = db.session.scalars(stmt_select_all_actors).all()
+        list_actors = [actor.short() for actor in actors]
+        if len(actors) == 0:
+            abort(404)
+        return jsonify({"success": True, "actors": list_actors})
+
     @app.route("/actors", methods=["POST"])
     def post_actor():
         try:
@@ -203,26 +230,24 @@ def create_app(test_config=None, db=db):
                 data_json_name is None
                 or data_json_age is None
                 or data_json_gender is None
+                or data_json_movies is None
             ):
                 abort(400)
 
-            if data_json_movies is None:
-                new_actor = Actor(
-                    name=data_json_name,
-                    age=data_json_age,
-                    gender=Gender(data_json_gender.capitalize()),
-                )
-            else:
+            movies = []
+
+            if len(data_json_movies) >= 0:
                 stmt_select_movies_by_ids = select(Movie).where(
                     Movie.id.in_(data_json_movies)
                 )
-                movies_by_ids = db.session.scalars(stmt_select_movies_by_ids).all()
-                new_actor = Actor(
-                    name=data_json_name,
-                    age=data_json_age,
-                    gender=Gender(data_json_gender.capitalize()),
-                    movies=movies_by_ids,
-                )
+                movies = db.session.scalars(stmt_select_movies_by_ids).all()
+
+            new_actor = Actor(
+                name=data_json_name,
+                age=data_json_age,
+                gender=Gender(data_json_gender.capitalize()),
+                movies=movies,
+            )
 
             db.session.add(new_actor)
             db.session.commit()
@@ -248,13 +273,32 @@ def create_app(test_config=None, db=db):
             if not data_json:
                 abort(400)
 
-            selected_actor.name = data_json.get("name", selected_actor.name)
-            selected_actor.age = data_json.get("age", selected_actor.age)
-            selected_actor.gender = data_json.get("gender", selected_actor.gender)
-            # TODO - first retrieve the MOVIES LIST to check if there is any before update the value
-            # selected_actor.movies = data_json.get("movies", selected_actor.movies)
-            selected_actor.gender = Gender(selected_actor.gender.capitalize())
+            data_json_name = data_json.get("name", None)
+            data_json_age = data_json.get("age", None)
+            data_json_gender = data_json.get("gender", None)
+            data_json_movies = data_json.get("movies", None)
 
+            if (
+                data_json_name is None
+                or data_json_age is None
+                or data_json_gender is None
+                or data_json_movies is None
+            ):
+                abort(400)
+
+            selected_actor.movies = []
+
+            if len(data_json_movies) > 0:
+                stmt_select_movies_by_ids = select(Movie).where(
+                    Movie.id.in_(data_json_movies)
+                )
+                selected_actor.movies = db.session.scalars(
+                    stmt_select_movies_by_ids
+                ).all()
+
+            selected_actor.name = data_json_name
+            selected_actor.age = data_json_age
+            selected_actor.gender = Gender(data_json_gender.capitalize())
             db.session.add(selected_actor)
             db.session.commit()
             return (
