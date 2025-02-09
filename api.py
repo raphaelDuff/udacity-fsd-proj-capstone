9986c32761c86a7bdb66e9e35488f2c4753705f7
raphaelDuff <raphaelprates@outlook.com>
2025-02-08 21:28:47 -0300

    Adding host ip for heroku

--- api.py
+++ api.py
@@ -393,4 +393,7 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(
+        os.environ.get("PORT", 5000)
+    )  # Get port from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
